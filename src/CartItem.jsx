5f016f562e27d7167b4200b2da6af4c9f570cf89
fgordon89 <franchesca.gordon@gmail.com>
2024-11-17 20:50:06 -0500

    haha

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -38,6 +38,8 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let cost = parseInt(item.cost.substring(1,item.cost.length));
+    return (cost * item.quantity);
   };
 
   return (
